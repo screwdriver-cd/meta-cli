af1578a51b8a05445ab3b8424e12d992b5e44733
St. John Johnson <st.john.johnson@gmail.com>
2018-02-05 22:08:21 -0800

    fix(habitat): Download the right dependencies (#15)

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -13,7 +13,9 @@ pkg_bin_dirs=(bin)
 
 # Scaffolding based on https://github.com/habitat-sh/core-plans/tree/master/scaffolding-go
 scaffolding_go_base_path="github.com/screwdriver-cd"
-scaffolding_go_build_deps=()
+scaffolding_go_build_deps=(
+    gopkg.in/urfave/cli.v1
+)
 
 # Extract the version from the last published GitHub release
 pkg_version() {
