87a144bef13307474fb66955f7d8c31e6e2b67f1
Sheridan C Rawlins <scr@verizonmedia.com>
2020-12-24 16:06:40 -0800

    Use tostring for better assert messages.

--- test-arg-passing-json.lua
+++ test-arg-passing-json.lua
@@ -2,7 +2,7 @@ local json = require('json')
 
 meta.set('myjson', json.decode(arg[1]))
 
-assert(meta.get('myjson.foo') == "bar", meta.get('myjson.foo'))
-assert(meta.get('myjson.bar[0]') == 1)
-assert(meta.get('myjson.bar[1]') == 2)
-assert(meta.get('myjson.bar[2]') == 3.45)
+assert(meta.get('myjson.foo') == "bar", tostring(meta.get('myjson.foo')))
+assert(meta.get('myjson.bar[0]') == 1, tostring(meta.get('myjson.bar[0]')))
+assert(meta.get('myjson.bar[1]') == 2, tostring(meta.get('myjson.bar[1]')))
+assert(meta.get('myjson.bar[2]') == 3.45, tostring(meta.get('myjson.bar[2]')))
